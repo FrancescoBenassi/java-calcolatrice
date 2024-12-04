3a80502a530a44fc7f30dbb1639b79c8b9a574d3
FrancescoBenassi <francybenna77@gmail.com>
2024-12-04 23:41:51 +0100

    feat: added methods static

--- src/org/lessons/java/utils/CalcoliHelper.java
+++ src/org/lessons/java/utils/CalcoliHelper.java
@@ -1,5 +1,53 @@
 package org.lessons.java.utils;
 
-public class CalcoliHelper {
+public final class CalcoliHelper {
+
+    public final static int sumNumber(int number1, int number2){
+        return number1 + number2;
+    }
+    
+    public final static double sumNumber(double number1, double number2){
+        return number1 + number2;
+    }
+
+    public final static int diffNumber(int number1, int number2){
+        return number1 - number2;
+    }
+
+    public final static double diffNumber(double number1, double number2){
+        return number1 - number2;
+    }
+
+    public final static int multiNumber(int number1, int number2){
+        return number1 * number2;
+    }
+
+    public final static double multiNumber(double number1, double number2){
+        return number1 * number2;
+    }
+
+    public final static int absNumber(int number){
+        return Math.abs(number);
+    }
+
+    public final static double absNumber(double number){
+        return Math.abs(number);
+    }
+
+    public final static int minNumber(int number1, int number2){
+        return Math.min(number1, number2);
+    }
+
+    public final static double minNumber(double number1, double number2){
+        return Math.min(number1, number2);
+    }
+
+    public final static int maxNumber(int number1, int number2){
+        return Math.max(number1, number2);
+    }
+
+    public final static double maxNumber(double number1, double number2){
+        return Math.max(number1, number2);
+    }
 
 }
